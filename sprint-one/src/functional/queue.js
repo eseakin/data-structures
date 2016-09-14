5550abc73f746a4c7fc5f571c083470d1fc2265c
eseakin <eseakin@gmail.com>
2016-09-14 15:16:26 -0700

    Queue almost complete. Need to add functionality for enqueue/dequeue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.dequeue = function() {
+
+    if (counter > 0) {
+      var result = $.extend({}, storage);
+
+
+      counter --;
+      return result[0];
+    }
   };
 
+
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
