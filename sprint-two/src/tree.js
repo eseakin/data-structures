1618eb3799ead867dee9a12718744c04aee33dfe
eseakin <eseakin@gmail.com>
2016-09-15 10:54:23 -0700

    Trees almost complete, One test to go. Nested children not searching properly.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,39 @@
 var Tree = function(value) {
+      console.log("new instance ");
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; 
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
+
+
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
+  // console.log(value, " added to ",this)
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    console.log("parent match");
+    return true;
+  }
+  if (this.children.length > 0) {
+    console.log(this.children);
+    for (var i = 0; i < this.children.length; i++) {
+      return this.children[i].contains(target);
+    }
+    //this.children.forEach(function(child) {       
+    //  return child.contains(target);
+    //});    
+  }
+  return false;
 };
 
 
