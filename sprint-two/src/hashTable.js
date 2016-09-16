5a69ec15a52e7cf7cdd4f2e90e2cd929066f61d7
eseakin <eseakin@gmail.com>
2016-09-15 19:38:10 -0700

    Finished Hashtables. All tests passed.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,25 +8,41 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(key, val) {
   var index = getIndexBelowMaxForKey(key, this._limit);
-  console.log(index,key);
-  if (this._storage.get(index) !== val) {
-    
-    index = getIndexBelowMaxForKey(key, this._limit);
+  console.log(index, key);
+  var obj = {}
+  obj[key] = val;
+  
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, obj);
+  } else {
+    obj = this._storage.get(index);
+    obj[key] = val;
+    this._storage.set(index, obj);
   }
-  this._storage.set(index, val);
-  console.log("storage = ",this._storage[index]);
+  
+  // storage[index] = {key:val}  
+
+
+
+  // if (this._storage.get(index) !== val) {
+
+  //   index = getIndexBelowMaxForKey(key, this._limit);
+  // }
+  // this._storage.set(index, val);
+  console.log("storage = ", this._storage[index]);
 };
 
 HashTable.prototype.retrieve = function(key) {
   var index = getIndexBelowMaxForKey(key, this._limit);
-  return this._storage.get(index);
+  var result = this._storage.get(index);
+  return result[key];
 };
 
 HashTable.prototype.remove = function(key) {
   var index = getIndexBelowMaxForKey(key, this._limit);
-  console.log(index, key)
-  console.log(this._storage)
-  this._storage.set(index, undefined);
+  console.log(index, key);
+  console.log(this._storage);
+  this._storage.set(index, {key: undefined});
 };
 
 
