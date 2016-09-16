9039bda37ff9791161c7469f3ad234c59a355cf1
eseakin <eseakin@gmail.com>
2016-09-16 10:45:38 -0700

    Finished all tests Binary Search Tree. All data structures complete!

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -5,15 +5,15 @@ var BinarySearchTree = function(startVal) {
 };
 
 BinarySearchTree.prototype.insert = function(newVal) {
-  if (this.value > newval) {
+  if (this.value > newVal) {
     if (this.left === null) {
-      this.left = BinarySearchTree(newVal);
+      this.left = new BinarySearchTree(newVal);
     } else {
       this.left.insert(newVal);
     }
-  } else if (this.value < newval) {
+  } else if (this.value < newVal) {
     if (this.right === null) {
-      this.right = BinarySearchTree(newVal);
+      this.right = new BinarySearchTree(newVal);
     } else {
       this.right.insert(newVal);
     }
@@ -22,13 +22,27 @@ BinarySearchTree.prototype.insert = function(newVal) {
 };
 
 BinarySearchTree.prototype.contains = function(findVal) {
-
-
+  if (this.value === findVal) {
+    return true;
+  } else if (this.value > findVal && this.left !== null) {
+    return this.left.contains(findVal)
+  } else if (this.value < findVal && this.right !== null) {
+    return this.right.contains(findVal)
+  }
+  return false; 
+  
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  this.value = cb(this.value);
 
+  if (this.left !== null) {
+    this.left.depthFirstLog(cb);
+  }
 
+  if (this.right !== null) {
+    this.right.depthFirstLog(cb);
+  } 
 };
 
 /*
